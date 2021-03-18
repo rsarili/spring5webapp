3f58ab2b666bfb52847e6cd8942c50ea6fbabd58
sarilir <rifat.sarili@commencis.com>
2021-03-18 21:30:34 +0300

    adding book view and thymeleaf

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -83,7 +83,6 @@ public class Book {
             ", title='" + title + '\'' +
             ", isbn='" + isbn + '\'' +
             ", publisher=" + publisher +
-            ", authors=" + authors +
             '}';
     }
 
